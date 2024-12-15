65df323f00cab215b799775fb615fe629568b4ea
john-kohout-e2g <jkohout@e2g.com>
2024-12-14 21:38:47 -0800

    feat: add handlers

--- src/lib/core/handler.zig
+++ src/lib/core/handler.zig
@@ -35,6 +35,60 @@ pub fn handle(day: u32, bside: bool, input: []const u8, allocator: std.mem.Alloc
         6 => {
             try day6.handle(buffer[0..end_index], bside, allocator);
         },
+        // 7 => {
+        //     try day7.handle(buffer[0..end_index], bside, allocator);
+        // },
+        // 8 => {
+        //     try day8.handle(buffer[0..end_index], bside, allocator);
+        // },
+        // 9 => {
+        //     try day9.handle(buffer[0..end_index], bside, allocator);
+        // },
+        // 10 => {
+        //     try day10.handle(buffer[0..end_index], bside, allocator);
+        // },
+        // 11 => {
+        //     try day11.handle(buffer[0..end_index], bside, allocator);
+        // },
+        // 12 => {
+        //     try da12.handle(buffer[0..end_index], bside, allocator);
+        // },
+        // 13 => {
+        //     try day13.handle(buffer[0..end_index], bside, allocator);
+        // },
+        // 14 => {
+        //     try day14.handle(buffer[0..end_index], bside, allocator);
+        // },
+        // 15 => {
+        //     try day15.handle(buffer[0..end_index], bside, allocator);
+        // },
+        // 16 => {
+        //     try day16.handle(buffer[0..end_index], bside, allocator);
+        // },
+        // 17 => {
+        //     try day17.handle(buffer[0..end_index], bside, allocator);
+        // },
+        // 18 => {
+        //     try day18.handle(buffer[0..end_index], bside, allocator);
+        // },
+        // 19 => {
+        //     try day19.handle(buffer[0..end_index], bside, allocator);
+        // },
+        // 20 => {
+        //     try day20.handle(buffer[0..end_index], bside, allocator);
+        // },
+        // 21 => {
+        //     try day21.handle(buffer[0..end_index], bside, allocator);
+        // },
+        // 22 => {
+        //     try day22.handle(buffer[0..end_index], bside, allocator);
+        // },
+        // 23 => {
+        //     try day23.handle(buffer[0..end_index], bside, allocator);
+        // },
+        // 24 => {
+        //     try day24.handle(buffer[0..end_index], bside, allocator);
+        // },
         else => {
             std.log.err("{d} is not a valid day", .{day});
         },
